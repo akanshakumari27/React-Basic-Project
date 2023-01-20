19d0090e873561ee30b8ab53f42366e980013fdf
Akansha <kumariakansha159@gmail.com>
2023-01-20 21:57:35 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Welcome from './components/Welcome';
+import { useState } from 'react';
+
 function App() {
+  const [onscreen, setOnscreen] = useState(false);
+
+  const [name , setName] = useState('')
+
+  const handleClick = event => {
+   
+    setOnscreen(true);
+
+  }
+  const handleChange = (event) => {
+    setName(event.target.value);
+    setOnscreen(false)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>React Assignment</h1>
+        <p>Enter your name: <input value={name} onChange={handleChange}>
+
+        </input> <button type='submit' onClick={handleClick}>Login</button></p>
+
+        {/* 👇️ show elements on click */}
+        {onscreen && <Welcome name={name}/>}
+        <p>Assignment done by Akansha Kumari (12020064) </p>
     </div>
   );
 }
